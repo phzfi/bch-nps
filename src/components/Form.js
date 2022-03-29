e16d2342a79bae6da7ebffd171c1d97c9cf7932c
Ele Na <elena.china-kolehmainen@edu.bc.fi>
2022-03-29 15:05:51 +0300

    CSS buttons instead of radio, experimental

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
 
+
+function range(start, end) {
+	return Array(end - start + 1).fill().map((_, idx) => ({
+		score: start + idx,
+		clicked: false}
+	))
+}
+	
+const radioArray = range(1, 10);
+
 const Form = () => {
 	const [score, setScore] = useState("");
-	const [count, setCount] = useState(0);
+	const [buttons, setButtons] = useState(radioArray);
 
-	const changeHandler = (e) => {
+	const clickHandler = (e) => {
 		e.preventDefault();
-		setScore(e.target.value);
-		setCount(1);
-		if (count === 1) e.target.style.backgroundColor = "#0000EE";
+		setButtons(prevState => prevState.map(button =>
+			button.score == e.target.textContent ? {...button, clicked: true} : {...button, clicked: false})
+		);
+		setScore(e.target.textContent);
 	};
 
 	return (
@@ -16,12 +27,9 @@ const Form = () => {
 			<h1>How likely are you to recommend us as an employer?</h1>
 			<p>On a scale of 1-10 (1=not likely, 10=very likely)</p>
 			<div className="buttons">
-				{Array(10)
-					.fill(1)
-					.map((val, i) => val + i)
-					.map((item) => (
-						<button key={item} onClick={changeHandler} value={item}>
-							{item}
+				{buttons.map((item) => (
+						<button key={item.score} id={item.clicked ? "active" : ""} onClick={clickHandler} value={item.score}>
+							{item.score}
 						</button>
 					))}
 			</div>

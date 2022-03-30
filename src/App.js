79f6a489d3f06893c31f1012935ef5a487f49746
Remu Ollinen <remu.ollinen@gmail.com>
2022-03-30 14:02:41 +0300

    made radio input form responsive for mobile, added popup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import Form from "./components/Form/Form";
 import MuiForm from "./components/MUIForm/MuiForm";
+import { useEffect, useState } from "react";
+import "./components/FormRadio/Form.css";
+import FormRadio from "./components/FormRadio/Form";
 
 function App() {
+	const [openModal, setOpenModal] = useState(false);
+
+	useEffect(() => {
+		setTimeout(() => setOpenModal(true), 2000);
+	}, []);
+
 	return (
 		<div className="App">
-			<Form />
+			{/* <Form /> */}
 			{/* <MuiForm /> */}
+			<h1>Promoter Score survey</h1>
+			{openModal && <FormRadio />}
 		</div>
 	);
 }

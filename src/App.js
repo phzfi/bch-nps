a8ec98909d05a0d957e6d1babd85467932555f80
Remu Ollinen <remu.ollinen@gmail.com>
2022-04-01 15:17:45 +0300

    added cookie checking to form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-// import Form from "./components/Form/Form";
+import Form from "./components/Form/Form";
 import MuiForm from "./components/MUIForm/MuiForm";
-// import "./components/FormRadio/Form.css";
-// import FormRadio from "./components/FormRadio/Form";
+import "./components/FormRadio/Form.css";
+import FormRadio from "./components/FormRadio/Form";
 
 function App() {
-	const [openModal, setOpenModal] = useState(false);
-
-	useEffect(() => {
-		setTimeout(() => setOpenModal(true), 2000);
-	}, []);
-
 	return (
 		<div className="App">
-		{/* <Form /> */}
-			{<MuiForm/>}
-			{/* <h1>Promoter Score survey</h1> 
-			{openModal && <FormRadio />}  */}
+			{/* <Form /> */}
+			{/* <MuiForm /> */}
+			{<FormRadio />}
 		</div>
 	);
 }

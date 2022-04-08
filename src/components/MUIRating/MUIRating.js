bdf29f64628efcd391c385a9ed3419996ccf6a80
Remu Ollinen <remu.ollinen@gmail.com>
2022-04-08 11:37:07 +0300

    added font, increased font size

--- src/components/MUIRating/MUIRating.js
+++ src/components/MUIRating/MUIRating.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import {
 	Box,
 	FormControl,
@@ -12,13 +12,12 @@ import {
 	DialogContent,
 	DialogTitle,
 	Container,
-	Alert
+	Alert,
 } from "@mui/material";
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
-import logo from '../assets/phz.png';
-import StarIcon from '@mui/icons-material/Star';
-
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+import logo from "../assets/phz.png";
+import StarIcon from "@mui/icons-material/Star";
 
 const MuiForm = () => {
 	const [surveyOpen, setSurveyOpen] = useState(true);
@@ -30,7 +29,7 @@ const MuiForm = () => {
 	const [thankyouOpen, setThankyouOpen] = useState(false);
 	const [hover, setHover] = useState(-1);
 	const theme = useTheme();
-	const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+	const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
 	useEffect(() => {
 		// info.map(i => console.log(i));
@@ -41,7 +40,6 @@ const MuiForm = () => {
 		checkACookieExists();
 		getResults();
 	}, [cookieFound, surveyOpen, error]);
-
 
 	// const options = {
 	//   enableHighAccuracy: true,
@@ -70,15 +68,17 @@ const MuiForm = () => {
 	// 	document.cookie,
 	// ]
 
-
 	// function error(err) {
 	// 	console.warn(`ERROR(${err.code}): ${err.message}`);
 	// }
 
-
 	function checkACookieExists() {
 		if (navigator.cookieEnabled) {
-			if (document.cookie.split(';').some((item) => item.trim().startsWith('-surveyAnsweringIcons-'))) {
+			if (
+				document.cookie
+					.split(";")
+					.some((item) => item.trim().startsWith("-surveyAnsweringIcons-"))
+			) {
 				setCookieFound(true);
 			}
 		}
@@ -92,7 +92,7 @@ const MuiForm = () => {
 		const expires = "; expires=" + date.toUTCString();
 		document.cookie = cookieName + "=" + cookieValue + expires + "; path=/";
 		setCookieFound(true);
-		console.log('status OK, 30 days cookie set');
+		console.log("status OK, 30 days cookie set");
 	}
 
 	// const handleClick = (e) => {
@@ -110,8 +110,7 @@ const MuiForm = () => {
 		setError(true);
 		setSurveyOpen(false);
 		setTimeout(() => setSurveyOpen(true), 5000);
-		
-	}
+	};
 
 	const handleCloseSurvey = () => {
 		setSurveyOpen(false);
@@ -126,159 +125,183 @@ const MuiForm = () => {
 	const handleCloseError = () => {
 		setError(false);
 		setTimeout(() => window.location.reload(), 2000);
-	}
+	};
 
 	const sendSurvey = () => {
-		axios.post('http://localhost:8080/api/reviews', {
-			score: score,
-			comment: comment,
-		})
-		.catch(err => errorInSubmit(err))
-		.then((res) => res.status === 200 ?  submitOK() : errorInSubmit());
+		axios
+			.post("http://localhost:8080/api/reviews", {
+				score: score,
+				comment: comment,
+			})
+			.catch((err) => errorInSubmit(err))
+			.then((res) => (res.status === 200 ? submitOK() : errorInSubmit()));
 	};
 
 	const getResults = () => {
-		axios.get('http://localhost:8080/api/reviews')
-		.then(res => console.log(res.data))
-		.catch(err => console.log(err));
+		axios
+			.get("http://localhost:8080/api/reviews")
+			.then((res) => console.log(res.data))
+			.catch((err) => console.log(err));
 	};
 
 	const labels = {
-		0: 'Not rated yet',
-		1: '1 star',
-		2: '2 stars',
-		3: '3 stars',
-		4: '4 stars',
-		5: '5 stars',
-		6: '6 stars',
-		7: '7 stars',
-		8: '8 stars',
-		9: '9 stars',
-		10: '10 stars',
-	  };
+		0: "Not rated yet",
+		1: "1 star",
+		2: "2 stars",
+		3: "3 stars",
+		4: "4 stars",
+		5: "5 stars",
+		6: "6 stars",
+		7: "7 stars",
+		8: "8 stars",
+		9: "9 stars",
+		10: "10 stars",
+	};
 
 	return (
 		<div>
-			{cookieFound && !thankyouOpen &&<Typography variant="h6" p={2}>
-			You already answered in the last 30 days. Clear your -surveyAnswering- cookie to test again.
-			</Typography>}
+			{cookieFound && !thankyouOpen && (
+				<Typography variant="h6" p={2}>
+					You already answered in the last 30 days. Clear your -surveyAnswering-
+					cookie to test again.
+				</Typography>
+			)}
 			{/* <Zoom > */}
-			{!cookieFound && <Dialog open={surveyOpen} 
+			{!cookieFound && (
+				<Dialog
+					open={surveyOpen}
 					onClose={handleCloseSurvey}
 					maxWidth="xl"
 					fullScreen={fullScreen}
-					>
-				<img
-					style={{ maxWidth: "10%", alignSelf: "center", paddingTop: "1.5rem" }}
-					src={logo}
-					alt="PHZ logo"
-				/>
-				<DialogTitle align="center">
-					How likely are you to recommend PHZ to a friend or colleague?
-				</DialogTitle>
-						<Typography
+				>
+					<img
+						style={{
+							maxWidth: "10%",
+							alignSelf: "center",
+							paddingTop: "1.5rem",
+						}}
+						src={logo}
+						alt="PHZ logo"
+					/>
+					<DialogTitle align="center" sx={{ fontSize: "1.8rem" }}>
+						How likely are you to recommend PHZ to a friend or colleague?
+					</DialogTitle>
+					<Typography
 						variant="subtitle1"
 						align="center"
 						color="textPrimary"
-						padding= "0"
-						margin= "0"
+						padding="0"
+						margin="0"
 					>
 						(0 = Not Likely, 10 = Very Likely)
 					</Typography>
-				<DialogContent align="center">
-					<FormControl className="centerForm" margin="dense" alignitems="center" sx={{display: "flex", justifyContent: "center"}}>
-						{/* <ButtonGroup variant="outlined" aria-label="outlined button group" fullWidth>
+					<DialogContent align="center">
+						<FormControl
+							className="centerForm"
+							margin="dense"
+							alignitems="center"
+							sx={{ display: "flex", justifyContent: "center" }}
+						>
+							{/* <ButtonGroup variant="outlined" aria-label="outlined button group" fullWidth>
 						{radioArray.map((item, i) => (
 								<Button key={i} onClick={handleClick} value={item.score}>{item.score}</Button>
 							))}
 
 
 						</ButtonGroup> */}
-						{/* {radioArray.map((item, i) => (
+							{/* {radioArray.map((item, i) => (
 								<Button key={i} onClick={handleClick} value={item.score}>{item.score}</Button>
 							))} */}
-							    <Box
-									sx={{
-										'& > legend': { mt: 2 },
-									}}
-									>
-									{score !== null && (
-        								<Box sx={{ml: 2, mb: "0.5rem"}}>
-											{labels[hover !== -1 ? hover : score]}
-										</Box>
-      								)}
-									<Rating
-										name="hover-feedback"
-										value={score}
-										emptyLabelText="not rate yet"
-										max={10}
-										size={"large"}
-										required
-										onChange={(event, newValue) => {
+							<Box
+								sx={{
+									"& > legend": { mt: 2 },
+								}}
+							>
+								{score !== null && (
+									<Box sx={{ ml: 2, mb: "0.5rem" }}>
+										{labels[hover !== -1 ? hover : score]}
+									</Box>
+								)}
+								<Rating
+									name="hover-feedback"
+									value={score}
+									emptyLabelText="not rate yet"
+									max={10}
+									sx={{ fontSize: "2rem" }}
+									required
+									onChange={(event, newValue) => {
 										setScore(newValue);
-										}}
-										onChangeActive={(event, newHover) => {
-											setHover(newHover);
-										}}
-										emptyIcon={<StarIcon style={{ opacity: 0.75 }} fontSize="inherit" />}
-									/>
-								</Box>
+									}}
+									onChangeActive={(event, newHover) => {
+										setHover(newHover);
+									}}
+									emptyIcon={
+										<StarIcon style={{ opacity: 0.75 }} fontSize="inherit" />
+									}
+								/>
+							</Box>
 
-						<Typography
-							paragraph
-							color="textPrimary"
-							align="center"
-							marginTop="1rem"
-							marginBottom="0rem"
-							paddingBottom="0rem"
-						>
-							Any comments? Ideas?
-						</Typography>
-						<TextareaAutosize
-							aria-label="empty textarea"
-							minRows={5}
-							sx={{marginTop: "0rem", padding: "0rem"}}
-							onChange={e => setComment(e.target.value)}
-						/>
-						<Container>
-							<Button
-								type="submit"
-								variant="contained"
-								color="primary"
-								sx={{minWidth: '100px',
-									m: 1
+							<Typography
+								paragraph
+								color="textPrimary"
+								align="center"
+								marginTop="1rem"
+								marginBottom="0rem"
+								paddingBottom="0rem"
+							>
+								Any comments? Ideas?
+							</Typography>
+							<TextareaAutosize
+								aria-label="empty textarea"
+								minRows={5}
+								sx={{
+									marginTop: "0rem",
+									padding: "0rem",
 								}}
-								onClick={sendSurvey}>
+								onChange={(e) => setComment(e.target.value)}
+							/>
+							<Container>
+								<Button
+									type="submit"
+									variant="contained"
+									color="primary"
+									sx={{ minWidth: "100px", m: 1 }}
+									onClick={sendSurvey}
+								>
 									Submit
-							</Button>
-							<Button
-								type="submit"
-								variant="outlined"
-								color="primary"
-								sx={{minWidth: '100px',
-								m: 1,
-								':hover': {
-									bgcolor: 'primary.main',
-									color: 'white',
-								}
-								}}
-								onClick={handleCloseSurvey}>
+								</Button>
+								<Button
+									type="submit"
+									variant="outlined"
+									color="primary"
+									sx={{
+										minWidth: "100px",
+										m: 1,
+										":hover": {
+											bgcolor: "primary.main",
+											color: "white",
+										},
+									}}
+									onClick={handleCloseSurvey}
+								>
 									Close
-							</Button>
-						</Container>
-					</FormControl>
-				</DialogContent>
-			</Dialog>}
+								</Button>
+							</Container>
+						</FormControl>
+					</DialogContent>
+				</Dialog>
+			)}
 			{/* </Zoom> */}
-			{thankyouOpen &&
-				(<Alert onClose={handleCloseThankyou}>
+			{thankyouOpen && (
+				<Alert onClose={handleCloseThankyou}>
 					Score <strong>submitted</strong>. Thank you for your feedback!
-				</Alert>)}
-			{error &&
-				(<Alert severity="error" onClose={handleCloseError}>
+				</Alert>
+			)}
+			{error && (
+				<Alert severity="error" onClose={handleCloseError}>
 					Something went wong. Did you forget to give a score?
-				</Alert>)}
-
+				</Alert>
+			)}
 		</div>
 	);
 };

17e12e3e63603bacb1a08e0c623ea4e5d4d96b60
Remu Ollinen <remu.ollinen@gmail.com>
2022-04-20 16:16:18 +0300

    data coming from database, added filtering, styles based on design

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,154 +6,195 @@ import "./Dashboard.css";
 const Dashboard = () => {
 	const [loading, setLoading] = useState(true);
 	const [reviews, setReviews] = useState([]);
-	const [sixMonthsReviews, setSixMonthsReviews] = useState([]);
-	const [sevenDayReviews, setSevenDaysReviews] = useState([]);
-	const [psSixMonths, setPsSixMonths] = useState(undefined);
-	const [psSevenDays, setPsSevenDays] = useState(undefined);
+	const [promoterScore, setPromoterScore] = useState({
+		score: 0,
+		promoters: 0,
+		passives: 0,
+		detractors: 0,
+	});
+	// const [sixMonthsReviews, setSixMonthsReviews] = useState([]);
+	// const [sevenDayReviews, setSevenDaysReviews] = useState([]);
+	// const [psSixMonths, setPsSixMonths] = useState(undefined);
+	// const [psSevenDays, setPsSevenDays] = useState(undefined);
 
 	useEffect(() => {
 		getReviews();
 	}, []);
 
-	useEffect(() => {
-		getSixMonthsReviews();
-		getSevenDaysReviews();
-		getPsSevenDays();
-		getPsSixMonths();
-	}, [
-		loading,
-		reviews,
-		sixMonthsReviews,
-		sevenDayReviews,
-		psSevenDays,
-		psSixMonths,
-	]);
+	// useEffect(() => {
+	// 	getSixMonthsReviews();
+	// 	getSevenDaysReviews();
+	// 	getPsSevenDays();
+	// 	getPsSixMonths();
+	// }, [
+	// 	loading,
+	// 	reviews,
+	// 	sixMonthsReviews,
+	// 	sevenDayReviews,
+	// 	psSevenDays,
+	// 	psSixMonths,
+	// ]);
 
 	const getReviews = () => {
 		axios
 			.get("http://localhost:8080/api/reviews")
 			.then((res) => {
 				setReviews(res.data);
+				setPromoterScore(calcPromoterScore(res.data));
 				setLoading(false);
 			})
 			.catch((err) => console.log(err));
 	};
 
-	const getSixMonthsReviews = () => {
-		const sixMonthsAgo = new Date();
-		sixMonthsAgo.setDate(sixMonthsAgo.getDate() - 120);
-		setSixMonthsReviews(
-			reviews?.filter((review) => Date.parse(review.createdAt) > sixMonthsAgo)
-		);
-	};
+	// const getSixMonthsReviews = () => {
+	// 	const sixMonthsAgo = new Date();
+	// 	sixMonthsAgo.setDate(sixMonthsAgo.getDate() - 120);
+	// 	setSixMonthsReviews(
+	// 		reviews?.filter((review) => Date.parse(review.createdAt) > sixMonthsAgo)
+	// 	);
+	// };
 
-	const getSevenDaysReviews = () => {
-		const sevenDaysAgo = new Date();
-		sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
-		setSevenDaysReviews(
-			reviews?.filter((review) => Date.parse(review.createdAt) > sevenDaysAgo)
-		);
-	};
+	// const getSevenDaysReviews = () => {
+	// 	const sevenDaysAgo = new Date();
+	// 	sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+	// 	setSevenDaysReviews(
+	// 		reviews?.filter((review) => Date.parse(review.createdAt) > sevenDaysAgo)
+	// 	);
+	// };
 
-	const getPsSevenDays = () => {
-		let respondants = sevenDayReviews.length;
+	// const getPsSevenDays = () => {
+	// 	let respondants = sevenDayReviews.length;
+	// 	let promoters = 0;
+	// 	let detractors = 0;
+	// 	for (let review of reviews) {
+	// 		if (review.score > 8) {
+	// 			promoters += 1;
+	// 		}
+	// 		if (review.score < 7) {
+	// 			detractors += 1;
+	// 		}
+	// 	}
+	// 	if (respondants) {
+	// 		setPsSevenDays(((promoters - detractors) / respondants) * 100);
+	// 	}
+	// };
+
+	// const getPsSixMonths = () => {
+	// 	let respondants = sixMonthsReviews.length;
+	// 	let promoters = 0;
+	// 	let passives = 0;
+	// 	let detractors = 0;
+	// 	for (let review of reviews) {
+	// 		if (review.score > 8) {
+	// 			promoters += 1;
+	// 		}
+	// 		if (review.score < 7) {
+	// 			detractors += 1;
+	// 		} else {
+	// 			passives += 1;
+	// 		}
+	// 	}
+	// 	if (respondants) {
+	// 		setPsSixMonths(((promoters - detractors) / respondants) * 100);
+	// 		setPsGroups({
+	// 			promoters: promoters,
+	// 			passives: passives,
+	// 			detractors: detractors,
+	// 		});
+	// 	}
+	// };
+
+	function calcPromoterScore(data) {
+		let respondants = data.length;
 		let promoters = 0;
+		let passives = 0;
 		let detractors = 0;
-		for (let review of reviews) {
+		for (let review of data) {
 			if (review.score > 8) {
 				promoters += 1;
 			}
 			if (review.score < 7) {
 				detractors += 1;
+			} else {
+				passives += 1;
 			}
 		}
 		if (respondants) {
-			setPsSevenDays(((promoters - detractors) / respondants) * 100);
+			return {
+				score: ((promoters - detractors) / respondants) * 100,
+				promoters: promoters,
+				passives: passives,
+				detractors: detractors,
+			};
 		}
+	}
+
+	const getPsByTimeSelection = (e) => {
+		const date = new Date();
+		date.setMonth(date.getMonth() - e.target.value);
+		const filteredReviews = reviews?.filter(
+			(review) => Date.parse(review.createdAt) > date
+		);
+		setPromoterScore(calcPromoterScore(filteredReviews));
+		console.log(date);
 	};
 
-	const getPsSixMonths = () => {
-		let respondants = sixMonthsReviews.length;
-		let promoters = 0;
-		let detractors = 0;
-		for (let review of reviews) {
-			if (review.score > 8) {
-				promoters += 1;
-			}
-			if (review.score < 7) {
-				detractors += 1;
-			}
-		}
-		if (respondants) {
-			setPsSixMonths(((promoters - detractors) / respondants) * 100);
-		}
-	};
+	// CHART DATA
 
 	const data = [
 		{
 			id: "Promoters",
 			label: "Promoters",
-			value: 56,
+			value: promoterScore.promoters,
 			color: "#3AC92E",
 		},
 		{
 			id: "Passives",
 			label: "Passives",
-			value: 28,
+			value: promoterScore.passives,
 			color: "#F7B055",
 		},
 		{
 			id: "Detractors",
 			label: "Detractors",
-			value: 12,
+			value: promoterScore.detractors,
 			color: "#ED6930",
 		},
 	];
 
-	const CenteredMetric = ({ dataWithArc, centerX, centerY }) => {
-		let total = 0;
-		dataWithArc.forEach((datum) => {
-			total += datum.value;
-		});
+	const CenteredMetric = ({ centerX, centerY }) => {
 		return (
 			<text
-				x={total > 0 ? centerX : centerX - 8}
+				x={promoterScore.score >= 0 ? centerX : centerX - 8}
 				y={centerY}
 				textAnchor="middle"
 				dominantBaseline="central"
 				style={{
-					fontSize: "3rem",
+					fontSize: "2.5rem",
 					fontWeight: 500,
 					fill: "#fff",
 				}}
 			>
-				{total}
+				{`${Math.ceil(promoterScore.score)}`}
 			</text>
 		);
 	};
 
 	return (
-		<div>
-			{/* <div className="score-box">
-				{loading && <h1>Loading promoter scores...</h1>}
-				<h1>PS of last 6 months: </h1>
-				{psSixMonths && (
-					<div className="score">
-						{(Math.round(psSixMonths * 100) / 100).toFixed(1)}
-					</div>
-				)}
-				<h1>PS of last 7 days: </h1>
-				{!loading && (
-					<div className="score">
-						{(Math.round(psSevenDays * 100) / 100).toFixed(1)}
-					</div>
-				)}
-			</div> */}
-			<div className="pie-wrapper">
-				<h2>Promoter Score</h2>
+		<div className="pie-wrapper">
+			<h2>Promoter Score</h2>
+			<select defaultValue="6" onChange={getPsByTimeSelection}>
+				<option value="1">Rolling 1 month</option>
+				<option value="2">Rolling 2 months</option>
+				<option value="3">Rolling 3 months</option>
+				<option value="4">Rolling 4 months</option>
+				<option value="5">Rolling 5 months</option>
+				<option value="6">Rolling 6 months</option>
+			</select>
+			{!loading && (
 				<ResponsivePie
 					data={data}
-					margin={{ top: 40, right: 60, bottom: 100, left: 60 }}
+					margin={{ top: 20, right: 80, bottom: 160, left: 80 }}
 					innerRadius={0.7}
 					padAngle={2}
 					cornerRadius={3}
@@ -162,10 +203,10 @@ const Dashboard = () => {
 					colors={{ datum: "data.color" }}
 					borderColor={{ from: "color", modifiers: [["darker", 2]] }}
 					enableArcLabels={false}
-					arcLinkLabel={(d) => `${d.value} ${d.id}`}
+					arcLinkLabel={(d) => `${d.value}`}
 					arcLinkLabelsSkipAngle={15}
 					arcLinkLabelsTextColor="#FFF"
-					arcLinkLabelsThickness={4}
+					arcLinkLabelsThickness={2}
 					arcLinkLabelsColor={{ from: "color" }}
 					arcLabelsSkipAngle={10}
 					arcLabelsTextColor={{ from: "color", modifiers: [["darker", 2]] }}
@@ -185,16 +226,16 @@ const Dashboard = () => {
 							translateY: 50,
 							itemsSpacing: 4,
 							itemWidth: 100,
-							itemHeight: 18,
+							itemHeight: 28,
 							itemTextColor: "#FFF",
-							itemDirection: "left-to-right",
+							itemDirection: "top-to-bottom",
 							itemOpacity: 1,
 							symbolSize: 20,
 							symbolShape: "circle",
 						},
 					]}
 				/>
-			</div>
+			)}
 		</div>
 	);
 };

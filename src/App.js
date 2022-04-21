c1a13f49f40e5e6b247e16f13cb15d862b24db89
Remu Ollinen <remu.ollinen@gmail.com>
2022-04-21 15:26:39 +0300

    new styles for navbar, removed property manager section

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
-import Layout from './Layout';
-import PropertyManager from './components/PropertyManager';
-import Dashboard from './components/Dashboard';
-import Account from './components/Account';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Dashboard from "./components/Dashboard";
+import Account from "./components/Account";
+import "./App.css";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route  index element={<Dashboard/>}/>
-          <Route path = "/propertymanager" element={<PropertyManager/>}/>
-          <Route path = "/account" element={<Account/>}/>
-        </Route>
-      </Routes>
-  </Router>
-  );
-}
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Dashboard />} />
+					<Route path="/account" element={<Account />} />
+				</Route>
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;

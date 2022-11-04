34d02cc444fd837912ceb5687316f7899df60313
hofnarr <hofnarr@hofnarr.fi>
2022-11-04 10:49:01 +0200

    add logic to show login modal only when user session is not present

--- admin-dashboard/src/components/LoginModal.js
+++ admin-dashboard/src/components/LoginModal.js
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
+
 import Modal from 'react-bootstrap/Modal';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {auth, authUiConfig} from "../firebase-config";
+
+import { onAuthStateChanged } from 'firebase/auth';
+
+import { auth, authUiConfig } from "../firebase-config";
 import StyledFireBaseAuth from "./StyledFireBaseAuth";
 
-const LoginModal =()=> {
-    const [show, setShow] = useState(true);
+const LoginModal = () => {
+    // right after page load the user session is almost always null.
+    // wait for the onAuthStateChanged() callback to get the real session state
+    
+    // let's show the modal only after we're sure the user session is not present
+    const [show, setShow] = useState(false);
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            console.log(`${user.email} (uid: ${user.uid}) signed in`)
+            setShow(false)
+        } else {
+            console.log("signed out")
+            setShow(true)
+        }
+    })
 
     return (
         <>
